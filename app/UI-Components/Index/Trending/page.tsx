2f131558843b9d1208184a6a82ca15f20a757b99
Jorge Matos <jisapjisap@gmail.com>
2025-12-21 11:36:30 +0100

    feat: add trending products UI component and sample product data

--- app/UI-Components/Index/Trending/page.tsx
+++ app/UI-Components/Index/Trending/page.tsx
@@ -1,4 +1,15 @@
-import React from 'react'
+"use client"
+
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Autoplay } from "swiper/modules"
+import "swiper/css"
+import Image from "next/image"
+import ProductData from "@/data/ProductsData.json"
+import { ToastContainer, toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import Link from "next/link"
+
+
 
 const Trending = () => {
   return (
@@ -20,6 +31,80 @@ const Trending = () => {
               View All
             </button>
           </div>
+        </div>
+
+        <div className="trending-swiper">
+          <Swiper
+            modules={[Autoplay]}
+            spaceBetween={20}
+            slidesPerView={4}
+            loop={true}
+            // autoplay={{
+            //   delay: 2500,
+            // }}
+            breakpoints={{
+              1200: {
+                slidesPerView: 4,
+              },
+              991: {
+                slidesPerView: 3,
+              },
+              575: {
+                slidesPerView: 1,
+              },
+              0: {
+                slidesPerView: 1,
+              }
+            }}
+          >
+            {ProductData.slice(0, 5).map((product, index) => (
+              <SwiperSlide key={index}>
+                <div className="product-card cursor-pointer">
+                  <div className="product-image">
+                    <Image
+                      src={product.image}
+                      alt={product.title}
+                      width={500}
+                      height={500}
+                      className="w-full h-full object-cover rounded-2xl"
+                    />
+
+                    <span className="absolute top-3 left-3 px-4 py-1 GolosText bg-white rounded-full">
+                      {product.off}
+                    </span>
+
+                    <div className="absolute top-5 right-5 flex flex-col gap-2">
+                      <i className="bi bi-balloon-heart product-icon w-10 h-10 flex items-center justify-center text-white bg-black/40 cursor-pointer rounded-full"></i>
+                      <i className="bi bi-cart3 product-icon w-10 h-10 flex items-center justify-center text-white bg-black/40 cursor-pointer rounded-full"></i>
+                    </div>
+
+                    <div className="absolute left-0 -bottom-1 lg:absolute lg:bottom-18 lg:left-18">
+                      <Link href={`/UI-Components/Shop/${product.id}`}>
+                        <button className="btn bg-black text-white cursor-pointer GolosText text-xl px-6 py-3 rounded-2xl w-full lg:w-auto lg:rounded-full border-3 border-white">
+                          View Details
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+
+                  <Link href={`/UI-Components/Shop/${product.id}`}>
+                    <div className="product-content mt-5 md:mt-10 z-10">
+                      <div className="flex justify-between">
+                        <h2 className="Lufga font-medium text-xl pr-5">
+                          {product.title}
+                        </h2>
+
+                        <h3 className="GolosText font-semibold text-2xl">
+                          {product.price}
+                        </h3>
+                      </div>
+                    </div>
+                  </Link>
+
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </div>
     </>
