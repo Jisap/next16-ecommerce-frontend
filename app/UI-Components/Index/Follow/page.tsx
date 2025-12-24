cdc4d9376edb8d7fc21c9617bb4afc6ab85a1d08
Jorge Matos <jisapjisap@gmail.com>
2025-12-24 18:05:59 +0100

    feat: introduce `Index` page component and `Follow` section for displaying Instagram content.

--- app/UI-Components/Index/Follow/page.tsx
+++ app/UI-Components/Index/Follow/page.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
 
-const page = () => {
+
+import Image from "next/image"
+import insta1 from "@/public/assets/insta-1.webp"
+import insta2 from "@/public/assets/insta-2.webp"
+import insta3 from "@/public/assets/insta-3.webp"
+import insta4 from "@/public/assets/insta-4.webp"
+import insta5 from "@/public/assets/insta-5.webp"
+import insta6 from "@/public/assets/insta-6.webp"
+import instaFollow from "@/public/assets/insta-follow.webp"
+
+
+const instaImage = [insta1, insta2, insta3, insta4, insta5, insta6]
+
+const Follow = () => {
   return (
-    <div>page</div>
+    <>
+      <div className="follow">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
+          {instaImage.map((insta, idx) => (
+            <div key={idx} className="follow-img">
+              <Image
+                src={insta}
+                alt="insta-images"
+              />
+            </div>
+          ))}
+        </div>
+
+        <div className="follow-text flex items-center gap-4 bg-white p-3 rounded-2xl">
+          <Image
+            src={instaFollow}
+            alt="insta-follow"
+            width={40}
+            height={40}
+          />
+
+          <h2 className="GolosText font-semibold text-xl">
+            Follow @Fashique
+          </h2>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default page
+export default Follow
--- app/UI-Components/Index/Index.tsx
+++ app/UI-Components/Index/Index.tsx
@@ -6,6 +6,7 @@ import Hero from "./Hero/page"
 import PopularProducts from "./PopularProducts/page"
 import Trending from "./Trending/page"
 import Blogs from "./Blogs/page"
+import Follow from "./Follow/page"
 
 
 
@@ -21,6 +22,7 @@ const Index = () => {
       <PopularProducts />
       <Companies />
       <Blogs />
+      <Follow />
     </>
   )
 }
