17a323953f23b190ef51768bd94eefdadabc8ce1
Jorge Matos <jisapjisap@gmail.com>
2026-01-01 18:02:03 +0100

    feat: add initial `Checkout` page component with cart item loading and total price calculation.

--- app/UI-Components/Pages/Chekout/page.tsx
+++ app/UI-Components/Pages/Chekout/page.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+"use client"
 
-const page = () => {
+import Image from "next/image"
+import Link from "next/link"
+import { toast, ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import Follow from "../../Index/Follow/page"
+import { useEffect, useState } from "react"
+import { CartItem } from "@/app/types/types"
+
+
+
+
+const Checkout = () => {
+
+  const [deliveryOption, setDeliveryOption] = useState<"ship" | "pickup">("ship");
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  useEffect(() => {
+    const saved = localStorage.getItem("cart");
+    if (saved) {
+      const parsed = JSON.parse(saved).map((item: CartItem) => ({
+        ...item,
+        qty: item.qty ?? 1
+      }));
+      setCartItems(parsed);
+    }
+  }, []);
+
+  const handlePlaceOrder = () => {
+    toast.success("Order placed successfully");
+    //localStorage.removeItem("cart");
+    //setCartItems([]);
+  }
+
+  const totalPrice = cartItems.reduce((acc, item) => {
+    const price = parseFloat(item.price.replace("$", "")) || 0;
+    const quantity = item.qty ?? 1;
+    return acc + price * quantity
+  }, 0)
+
   return (
-    <div>page</div>
+    <>
+
+    </>
   )
 }
 
-export default page
+export default Checkout
