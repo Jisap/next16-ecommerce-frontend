c18e53dd23317ee7792be35f8f1c3dde88b08b81
Jorge Matos <jisapjisap@gmail.com>
2026-01-02 20:40:12 +0100

    feat: create dynamic blog details page for individual blog posts

--- app/UI-Components/Blog/[id]/page.tsx
+++ app/UI-Components/Blog/[id]/page.tsx
@@ -1,9 +1,35 @@
+import PageHeader from "@/app/components/PageHeader"
+import BlogData from "@/data/BlogsData.json"
+import Image from "next/image"
+import Link from "next/link"
+import { useParams } from "next/navigation"
 
 
-const page = () => {
+
+const BlodDetails = () => {
+
+  const params = useParams();
+  const id = params?.id;
+
+  const blog = BlogData.find((blog) => blog.id === Number(id));
+  if (!blog) {
+    return (
+      <div className="py-20 text-center text-3xl font-bold">
+        Blog Not Found
+      </div>
+    )
+  }
+
   return (
-    <div>page</div>
+    <>
+      <PageHeader
+        title="Blog Details"
+        currentPage="Blog Details"
+        parentPage="Blog"
+        parentLink="/UI-Components/Blog"
+      />
+    </>
   )
 }
 
-export default page
+export default BlodDetails
